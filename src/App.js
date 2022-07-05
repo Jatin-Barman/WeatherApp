2b17642e1f1fafdebf3d608140030ac0a16711f1
Jatin-Barman <82039294+Jatin-Barman@users.noreply.github.com>
2022-07-06 00:57:42 +0530

    Update App.js
    deleted my api key security reasons

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
 
   const getWeatherInfo = async () => {
     try {
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=086d6a0ec5086e422b278bcfbe44a94a`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid={key}`;
 
       let res = await fetch(url);
       let data = await res.json();
